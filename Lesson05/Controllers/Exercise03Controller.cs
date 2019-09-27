ece38508752cbe2af5f6ce38874de548fd8e7af0
petrmrkvicka <petr@petrmrkvicka.com>
2019-09-27 14:36:08 +0200

    receipt

--- Lesson05/Controllers/Exercise03Controller.cs
+++ Lesson05/Controllers/Exercise03Controller.cs
@@ -11,17 +11,23 @@ namespace Lesson05.Controllers
 {
     public class Exercise03Controller : Controller
     {
+        [HttpPost]
+        public IActionResult Index(BreakfastOrder breakfastOrder)
+        {
+            return View("Receipt", breakfastOrder);
+        }
+
         // GET: /<controller>/
         public IActionResult Index()
         {
             BreakfastOrder breakfastOrder = new BreakfastOrder();
-            breakfastOrder.AddBreakfastFood(new BreakfastFood { BreakfastFoodId = 1, Name = "Cornflakes", Price = 36, Selected = false });
-            breakfastOrder.AddBreakfastFood(new BreakfastFood { BreakfastFoodId = 2, Name = "Egg", Price = 7, Selected = false });
-            breakfastOrder.AddBreakfastFood(new BreakfastFood { BreakfastFoodId = 3, Name = "Toast", Price = 18, Selected = false });
-            breakfastOrder.AddBreakfastFood(new BreakfastFood { BreakfastFoodId = 4, Name = "Juice", Price = 40, Selected = false });
-            breakfastOrder.AddBreakfastFood(new BreakfastFood { BreakfastFoodId = 5, Name = "Milk", Price = 20, Selected = false });
-            breakfastOrder.AddBreakfastFood(new BreakfastFood { BreakfastFoodId = 6, Name = "Coffee", Price = 25, Selected = false });
-            breakfastOrder.AddBreakfastFood(new BreakfastFood { BreakfastFoodId = 7, Name = "Tea", Price = 32, Selected = false });
+            breakfastOrder.AddBreakfastFood(new BreakfastFood { BreakfastFoodId = 1, Name = "Cornflakes", Price = 36M, Selected = false });
+            breakfastOrder.AddBreakfastFood(new BreakfastFood { BreakfastFoodId = 2, Name = "Egg", Price = 7M, Selected = false });
+            breakfastOrder.AddBreakfastFood(new BreakfastFood { BreakfastFoodId = 3, Name = "Toast", Price = 18M, Selected = false });
+            breakfastOrder.AddBreakfastFood(new BreakfastFood { BreakfastFoodId = 4, Name = "Juice", Price = 40M, Selected = false });
+            breakfastOrder.AddBreakfastFood(new BreakfastFood { BreakfastFoodId = 5, Name = "Milk", Price = 20M, Selected = false });
+            breakfastOrder.AddBreakfastFood(new BreakfastFood { BreakfastFoodId = 6, Name = "Coffee", Price = 25M, Selected = false });
+            breakfastOrder.AddBreakfastFood(new BreakfastFood { BreakfastFoodId = 7, Name = "Tea", Price = 32M, Selected = false });
 
             return View(breakfastOrder);
         }
